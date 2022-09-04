ecb6be3de27c13052f986c981f3c62f81846f983
thanhtutzaw <zgyi5025@gmail.com>
2022-09-04 19:55:10 +0630

    some padding and profile

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import image from '../public/1.png'
 
 export default function Home() {
   return (
-      <>
+    <>
       <Head>
         <title>ThanHtutZaw</title>
         <meta name="description" content="Web Dev" />
@@ -14,30 +15,32 @@ export default function Home() {
       <main className={styles.main}>
         <ul className={styles.cardsContainer}>
 
-          <li className={[styles.card , styles.card1].join(" ")}>d</li>
-          <li className={[styles.card , styles.card2].join(" ")}>d</li>
-          <li className={[styles.card , styles.card3].join(" ")}>d</li>
-          <li className={[styles.card, styles.d4 , styles.card4].join(" ")}>
-            <div className={[styles.d4_face,styles.d4_front].join(" ")}>React</div>
-            <div className={[styles.d4_face,styles.d4_back].join(" ")}>Next</div>
-            <div className={[styles.d4_face,styles.d4_right].join(" ")}>SCSS</div>
-            <div className={[styles.d4_face,styles.d4_left].join(" ")}>TypeScript</div>
-            <div className={[styles.d4_face,styles.d4_top].join(" ")}>Firebase</div>
-            <div className={[styles.d4_face,styles.d4_bottom].join(" ")}>Nest</div>
-            
-            
+          <li className={[styles.card, styles.card1].join(" ")}>
+            <Image className={styles.image} width={200} height={220} layout='fixed' src={image} alt="profile"></Image>
+          </li>
+          <li className={[styles.card, styles.card2].join(" ")}>2</li>
+          <li className={[styles.card, styles.card3].join(" ")}>3</li>
+          <li className={[styles.card, styles.card4].join(" ")}>
+            <div className={[styles.d4_face, styles.d4_front].join(" ")}>React</div>
+            <div className={[styles.d4_face, styles.d4_back].join(" ")}>Next</div>
+            <div className={[styles.d4_face, styles.d4_right].join(" ")}>SCSS</div>
+            <div className={[styles.d4_face, styles.d4_left].join(" ")}>TypeScript</div>
+            <div className={[styles.d4_face, styles.d4_top].join(" ")}>Firebase</div>
+            <div className={[styles.d4_face, styles.d4_bottom].join(" ")}>Nest</div>
+
+
             {/* <li className={[ styles.card4].join(" ")}>d</li> */}
             {/* <li className={[ styles.card5].join(" ")}>ccc</li> */}
           </li>
-          
-         
-          
+
+
+
 
 
         </ul>
       </main>
-      </>
+    </>
 
-      
+
   )
 }

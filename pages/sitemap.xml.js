fc2873364b7ebba7a5a5314bcb2ca32f84faa86a
thanhtutzaw <zgyi5025@gmail.com>
2024-01-28 17:56:32 +0630

    update : sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,18 +1,18 @@
 
 const URL = "https://thz.vercel.app"
 export function generateSiteMap() {
-    // const posts = getSortedPostsData.map(({ id, date }) => ({
-    //     url: `${URL}/blog/${id}`,
-    //     lastModified: date,
-    // }));
+  // const posts = getSortedPostsData.map(({ id, date }) => ({
+  //     url: `${URL}/blog/${id}`,
+  //     lastModified: date,
+  // }));
 
-    // const routes = ["", "/hello"].map((route) => ({
-    //     url: `${URL}${route}`,
-    //     lastModified: new Date().toISOString(),
-    // }));
+  // const routes = ["", "/hello"].map((route) => ({
+  //     url: `${URL}${route}`,
+  //     lastModified: new Date().toISOString(),
+  // }));
 
-    // return [...routes];
-    return `<xml version="1.0" encoding="UTF-8">
+  // return [...routes];
+  return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
      <!-- Add the static URLs manually -->
      <url>
@@ -25,27 +25,27 @@ export function generateSiteMap() {
        <loc>${URL}/blog</loc>
      </url>
             ${["/hello"].map((route) => {
-        return `
+    return `
             <url><loc>${URL}${route}</loc></url>
             `}).join("")}
-   </urlset></xml>
+   </urlset></?xml>
  `;
 }
 
 export async function getServerSideProps({ res }) {
-    // const posts = getSortedPostsData();
+  // const posts = getSortedPostsData();
 
-    // Generate the XML sitemap with the blog data
-    const sitemap = generateSiteMap();
+  // Generate the XML sitemap with the blog data
+  const sitemap = generateSiteMap();
 
-    res.setHeader("Content-Type", "text/xml");
-    // Send the XML to the browser
-    res.write(sitemap);
-    res.end();
+  res.setHeader("Content-Type", "text/xml");
+  // Send the XML to the browser
+  res.write(sitemap);
+  res.end();
 
-    return {
-        props: {},
-    };
+  return {
+    props: {},
+  };
 }
 
 export default function SiteMap() { }
